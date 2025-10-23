3d0d2da3f29a401650444405ea4c7f778ab325d2
adham mohamed <adhamdr10@gmail.com>
2025-10-23 20:21:51 +0300

    Add CourseDTO

---
+++ Day 1/DTO/CourseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_1.DTO
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+    }
+}
